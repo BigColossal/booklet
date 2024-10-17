99c0265f6c89ba79bc0cd01a1164db0f448e4414
BigColossal <jeremyperez1409@gmail.com>
2024-10-17 18:08:59 -0400

    Added a report to be printed when run

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_amount = get_word_amount(text)
-    letter_quantities = get_letter_amount()
+    letter_quantities = get_letter_amount(text)
+    sorted_dict = sort_dict(letter_quantities)
+
     print(text)
-    print(f"\n{word_amount} words were found in this book!")
-    print(f"\n{letter_quantities}")
+    print(f"\n ^-----Report of {book_path}-----^")
+    print(f"\n{word_amount} words were found in this book!\n")
+    for letter, count in sorted_dict:
+        print(f"The '{letter}' character was found {count} times")
+    print("-------------End of report-------------")
+    
 
-
+def sort_dict(dict):
+    return sorted(dict.items(), key=lambda item: item[1], reverse=True)
+    
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -18,22 +26,15 @@ def get_word_amount(text):
     words = lowered_text.split()
     return len(words)
 
-def get_letter_amount():
+def get_letter_amount(text):
     letter_quantities = {}
-    word_list = transport_words_to_list()
-    for word in word_list:
+    for word in text.lower():
         for letter in word:
-            if letter not in letter_quantities:
-                letter_quantities[letter] = 1
-            else:
-                letter_quantities[letter] += 1
+            if letter.isalpha():
+                if letter not in letter_quantities:
+                    letter_quantities[letter] = 1
+                else:
+                    letter_quantities[letter] += 1
     return letter_quantities
-
-
-def transport_words_to_list():
-    word_list = []
-    for word in lowered_text.split():
-        word_list.append(word)
-    return word_list
 
 main()
